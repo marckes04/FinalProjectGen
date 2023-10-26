2e7086c9a0c46d482d49a2a7355ee89d46c76cfb
barakei-26 <barake883@gmail.com>
2023-10-26 16:40:22 -0500

    UI NPC AND NAVMESH PROTOTYPE

---
+++ Assets/Scripts/ZombieMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    [SerializeField] private float guardRange;
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        GameObject player = GameObject.Find("MorlenSprite");
+        playerTransform = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: Add Attack Logic
+        if (playerTransform != null && Vector3.Distance(playerTransform.position,transform.position)<guardRange) 
+        { 
+        agent.destination = playerTransform.position;
+        }
+    }
+}

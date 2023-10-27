ec6b55ad6322915781bc4413e3f8990e3bb3a697
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2023-10-27 01:32:23 -0500

    shoot directional added

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -4,29 +4,18 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public Transform bulletSpawnPoint;
-    public GameObject bulletPrefab;
-    public float bulletSpeed;
-    [SerializeField] private int bulletAngle = -45;
-    [SerializeField] private int bulletAngle2 = 45;
-    private Rigidbody2D myBullet;
-
-
-    void Update()
+    public GameObject fireBullet;
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        ShootBullet();
+    }
 
-
-            //var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            //bullet.GetComponent<Rigidbody2D>().velocity = bulletSpeed * new Vector2(-Mathf.Cos(-bulletAngle * Mathf.Deg2Rad), Mathf.Sin(- bulletAngle * Mathf.Deg2Rad));
-
-            //var bullet2 = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            //bullet2.GetComponent<Rigidbody2D>().velocity = bulletSpeed * new Vector2(Mathf.Cos(bulletAngle2 * Mathf.Deg2Rad), Mathf.Sin(bulletAngle2 * Mathf.Deg2Rad));
-
-            var bullet3 = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet3.GetComponent<Rigidbody2D>().velocity = bulletSpeed * Vector2.right;
+    void ShootBullet()
+    {
+        if (Input.GetKeyDown (KeyCode.J)) 
+        { 
+            GameObject bullet = Instantiate(fireBullet, transform.position, Quaternion.identity);
+            bullet.GetComponent<FireBullet>().Speed *= transform.localScale.x;
         }
-
     }
 }

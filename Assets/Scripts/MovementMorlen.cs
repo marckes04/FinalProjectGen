a72709e33a5d818f857d752e72583ff86ea89dba
Snowdencd <snow-0202@hotmail.com>
2023-10-27 00:11:33 -0500

    Shadow
    Sprites with shadow and change to night

--- Assets/Scripts/MovementMorlen.cs
+++ Assets/Scripts/MovementMorlen.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class MovementMorlen : MonoBehaviour
 {
     public float speed;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,7 +18,13 @@ public class MovementMorlen : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput ) * speed * Time.deltaTime;
         transform.Translate(movement);
+
+        float movementMagnitude = movement.magnitude;
+
+        bool isMovingHorizontally = Mathf.Abs(horizontalInput) > 0.1f;
+        animator.SetBool("isWalk", isMovingHorizontally);
     }
 }
